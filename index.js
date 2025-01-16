4dce0cc92f9d8ae056dd8074efb7cb480cf93ff2
jatobro <jakob.tossebro@gmail.com>
2025-01-17 00:27:12 +0100

    react to bandle channel

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  console.log(`ready! logged in as ${readyClient.user.tag}`);
 });
 
 client.login(Bun.env.TOKEN);
+
+client.on(Events.MessageCreate, (message) => {
+  if (message.author.bot) return;
+
+  if (message.channelId != Bun.env.BANDLE_ID) return;
+
+  console.log(
+    `message sent to bandle channel with content:\n${message.content}`
+  );
+
+  const bandleRegex = /^Bandle #\d+ [1-6]\/6/;
+
+  console.log(bandleRegex.test(message.content));
+
+  if (!bandleRegex.test(message.content)) return;
+
+  message
+    .react("bandle score posted")
+    .then(() =>
+      console.log(`Reacted to a Bandle message from ${message.author.tag}`)
+    )
+    .catch(console.error);
+});
